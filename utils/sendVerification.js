6308d2cd1ed17d89e2340a5afae3654bc55aa1dc
DevWithEasy <devwitheasy@gmail.com>
2022-10-17 18:00:39 +0600

    start command

--- utils/sendVerification.js
+++ utils/sendVerification.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
 
-const sendVerification = () =>{
+const sendVerification = (reciever) =>{
+  //CREATE TRANSPORTER
   const transporter = nodemailer.createTransport({
     service:"gmail",
     auth:{
@@ -8,6 +9,18 @@ const sendVerification = () =>{
       pass : process.env.PASSWORD
     }
   })
+
+//CREATE MAIL OPTIONS
+const mailOptions = {
+  from : "devwitheasy@gmail.com",
+  to : reciever,
+  subject : "FoodCommunityBD Account verifiaction",
+  html : `
+      <p>Please verify your acount from <a href="http://www.foodcommunity."/> here</p>
+      <p>This link with expire after 06 hours later.</p>
+  `
+}
+
 }
 
 module.exports = sendVerification;
